92e7e788b410667d7a370067d992f42509e06f44
g1344 <guoyz@dtdream.com>
2019-07-20 15:54:57 +0800

    'init component branch which for hosting the basic components'

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# banana
+# branch of component 
+
+this branch is mainly for the basic component for bigger projects.
---
+++ src/test/java/com/banana/Test.java
@@ -0,0 +1,12 @@
+package com.banana;
+
+/**
+ * Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("中文`");
+    }
+
+    
+}
