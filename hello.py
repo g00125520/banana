a8aefa6e177a93327625984c07ee9cdc4c085f61
g1344 <guoyz@dtdream.com>
2020-06-18 16:25:18 +0800

    'test time package'

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
-msg = "hello world!"
+import time
 
-print(msg.capitalize)
+dt = "2020-06-17 00:00:00"
+ts = time.strptime(dt, "%Y-%m-%d %H:%M:%S")
+tt = time.mktime(ts)
+print( tt )
+print(round( tt * 1000))
+print(int(round(tt * 1000)))
+print(time.time())
+print( time.time() * 1000)
+print( round( time.time() * 1000) )
